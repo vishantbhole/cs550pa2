ae02f43a039eec230fdd54736dd6e5d92b9297ba
Saptarshi Chatterjee <diesel@falcon.local>
2018-10-13 21:48:26 -0500

    first

---
+++ src/com/iit/MessageFormat.java
@@ -0,0 +1,11 @@
+package com.iit;
+
+import java.io.Serializable;
+
+public class MessageFormat implements Serializable {
+
+    String fname;
+    String msgId;
+    int fromPeerId;
+    int TTL_value;
+}
